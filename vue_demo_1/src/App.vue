9b438dbe8d0456fe6a4347d5171b7db5d661fd60
ธีรธัช ถวิลหวัง <ten.teeratach@gmail.com>
2022-03-06 11:06:36 +0700

    Create project files
    Create project files.

---
+++ vue_demo_1/src/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <h1>test</h1>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+* {
+  margin: 0px;
+  padding: 0px;
+}
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+}
+</style>
